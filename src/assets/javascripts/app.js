86257ad9cd8797d810c5469f825058e23ca6c61b
toyama satoshi <toyamarinyon@gmail.com>
2016-01-27 18:55:50 +0900

    Pass eslint

--- src/assets/javascripts/app.js
+++ src/assets/javascripts/app.js
@@ -1,43 +1,42 @@
-import $ from 'jquery'
+import $ from 'jquery';
 
-$( () => {
-  const $body = $('body')
-  const $language_navigators = $('.js--language-navigator li')
-  const $language_articles = $('.js--article')
-  const $page_scrollers = $('.js--page-scroller')
-  const changeArticle = (read_language) => {
-    $language_articles.hide()
-    $language_articles
-      .filter(`.js--article--language-${read_language}`)
-      .show()
-  }
-  $language_navigators.on('click', (event) => {
-    event.preventDefault()
-    $language_navigators.removeClass('active')
-    const read_language = $(event.currentTarget).data('language')
-    $(event.currentTarget).addClass('active')
-    changeArticle(read_language)
-  })
-  changeArticle('ruby')
+$(() => {
+  const $body = $('body');
+  const $languageNavigators = $('.js--language-navigator li');
+  const $languageArticles = $('.js--article');
+  const $pageScrollers = $('.js--page-scroller');
+  const changeArticle = (readLanguage) => {
+    $languageArticles.hide();
+    $languageArticles
+      .filter(`.js--article--language-${readLanguage}`)
+      .show();
+  };
+  $languageNavigators.on('click', (event) => {
+    event.preventDefault();
+    $languageNavigators.removeClass('active');
+    const readLanguage = $(event.currentTarget).data('language');
+    $(event.currentTarget).addClass('active');
+    changeArticle(readLanguage);
+  });
+  changeArticle('ruby');
 
-  $page_scrollers.on('click', (event) => {
-    event.preventDefault()
-    const scroll_to = $(event.currentTarget).data('scrollTo')
-    if(scroll_to === 'top') {
-      $body.animate({ scrollTop: 0 }, 200)
+  $pageScrollers.on('click', (event) => {
+    event.preventDefault();
+    const scrollTo = $(event.currentTarget).data('scrollTo');
+    if (scrollTo === 'top') {
+      $body.animate({ scrollTop: 0 }, 200);
+    } else if (scrollTo === 'bottom') {
+      $body.animate({ scrollTop: $(window).height() }, 200);
     }
-    else if(scroll_to === 'bottom') {
-      $body.animate({ scrollTop: $(window).height() }, 200)
-    }
-    $(event.currentTarget).blur()
-  })
+    $(event.currentTarget).blur();
+  });
 
   // $(window).on('scroll', (event) => {
   //   if($(window).scrollTop() < 60) {
-  //     $page_scrollers.fadeOut()
+  //     $pageScrollers.fadeOut()
   //   }
-  //   else if($page_scrollers.is(':hidden')) {
-  //     $page_scrollers.fadeIn()
+  //   else if($pageScrollers.is(':hidden')) {
+  //     $pageScrollers.fadeIn()
   //   }
   // })
-})
+});
